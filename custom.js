dd70b96ccb06f0d1ec3739fb8cbc5b89dc0e9963
山口浩太郎 <kotaro@yamaguchikoutarounoMacBook.local>
2019-08-29 13:41:19 +0900

    add custom.js

---
+++ custom.js
@@ -0,0 +1,5 @@
+(function(d,j){
+  j = d.createElement('script');
+  j.src = '//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+  (d.head || d.getElementsByTagName('head')[0]).appendChild(j);
+  }(document));
